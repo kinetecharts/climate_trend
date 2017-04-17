f73ca8cf987b9185d45aa734a3cdeffee9800f22
donkimber <kimber@fxpal.net>
2017-04-17 13:27:43 -0700

    new version with side panel and year counter in upper corner

---
+++ sand/js/app_v2.js
@@ -0,0 +1,443 @@
+"use strict";
+
+var qstr = location.search.substring(1);
+var qsObj = {};
+if (qstr) {
+    try {
+	qsObj = JSON.parse('{"' + decodeURI(qstr).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+    }
+    catch (e) {
+	alert("Bad query string in URL");
+    }
+}
+
+function getParameter(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+//for (var key in params) {
+console.log("------------------------------------");
+for (var key in qsObj) {
+    var str = qsObj[key];
+    console.log(" "+key+": "+str);
+    //var str = getParameter(key);
+    if (str) {
+	if (params[key] == null) {
+	    alert("Bad param "+key);
+	}
+	try {
+	    params[key] = eval(str);
+	}
+	catch(e) {
+	    alert("Bad element in query string: "+str);
+	}
+    }
+}
+console.log("------------------------------------");
+
+const Parameters = ['temperature', 'co2', 'ice', 'balance', 'precipitation']
+
+const PreIndustrial = {
+	temperature : 13.36,
+	ice: 7.78,
+	co2: 284.7,
+	balance: 0,
+	precipitation: 3.38E-05
+}
+
+let backgroundColor = 0x101025
+
+var mouseX = 0, mouseY = 0;
+
+window.__ = _.noConflict();
+
+
+const numData = 451
+
+const chartRange={
+	x:[1850, 2300],
+	y:[12, 24],
+	z:[-5, 5]
+}
+
+
+var Year = 1850
+var SandYear = 1850
+
+const chartScale=[1.5,1,1.5]
+
+var colors = {
+  x: new THREE.Color(0xFF4136),
+  y: new THREE.Color(0x2ECC40),
+  z: new THREE.Color(0x0074D9)
+};
+
+// loadData('data/rcp8p5.csv')
+let dataFiles = ['../data/rcp8p5.csv', '../data/rcp2p6.csv']
+Q.all(dataFiles.map(f=>{return loadData(f)}))
+	.then(data=>{
+		console.log("done data loading")
+		var _data = {
+			'rcp8p5': processData(data[0], numData),
+			'rcp2p6': processData(data[1], numData),
+			'active': processData(data[0], numData)
+		}
+		window._data = _data
+		return _data
+	})
+	.then(d=>{
+		var numPasses = 5
+		for(var i=0; i<numPasses; i++){
+			d=smoothEnergyBalance(d)
+		}
+		return d
+	})
+	.then(d=>{
+		draw(d)
+		runEvents(events_normal)
+	})
+	.fail(err=>{
+		console.log(err)
+	})
+
+// set active = r*rcp8.5 + (1-r)*rcp2.6
+var setActiveData = (data, r) =>{
+	for(var i=0; i<numData; i++){
+		Parameters.forEach(p=>{
+			data.active[p][i] = data.rcp8p5[p][i] * r + data.rcp2p6[p][i] * (1.0-r)
+		})
+	}
+}
+
+var drawAxis = (view, origin)=>{
+    var xticks = 6
+
+    // X axis
+	view
+	.transform({
+		position:[0, origin.y, origin.z]
+	})
+	.axis({
+	  axis: "x", // year
+	  end: true,
+	  width: 6,
+	  depth: 1,
+	  color: colors.x,
+	  opacity: 1.0,
+	})
+	
+	view.scale({
+      divide: 5,
+      nice: false,
+      origin: [1850, 12, 0, 0],
+      axis: "x"
+    })
+    .ticks({
+      classes: ['foo', 'bar'],
+      width: 20
+    })
+    .text({
+    	live: false,
+    	data: interpolate(chartRange.x[0], chartRange.x[1], 6)
+    })
+    .label({
+    	color: 0xaaaaaa,
+    	background: backgroundColor,
+    	size: 36,
+    	snap: false,
+    	depth: 1
+    	// offset: [1,1]
+    })
+
+    // Y axis
+	view.transform({
+		position:[origin.x, 0, origin.z]
+	})
+	.axis({
+	  axis: "y",
+	  end: true,
+	  width: 3,
+	  depth: 1,
+	  color: colors.y,
+	  opacity: .5,
+	})
+
+	// Z axis
+	view.transform({
+		position: [origin.x, origin.y, ]
+	})
+	.axis({
+	  axis: "z",
+	  end: true,
+	  width: 3,
+	  depth: 1,
+	  color: colors.z,
+	  opacity: .5,
+	});
+
+	// XyZ labela
+    view.array({
+      id: "colors",
+      live: false,
+      data: [colors.x, colors.y, colors.z].map(function (color){
+        return [color.r, color.g, color.b, 1];
+      }),
+    });
+
+    view.array({
+      data: [[2350,origin.y,origin.z], [1850,25,0], [1850,12,10]],
+      channels: 3, // necessary
+      live: false,
+    }).text({
+      data: ["year", "y", "z"],
+    }).label({
+      color: 0xaaaaaa,
+      background: backgroundColor,
+      size: 48,
+      depth: 1
+    });		
+
+}
+
+var drawGrid = (view, origin)=>{
+	//const lineWidth = 1
+	const lineWidth = params.gridLineWidth
+	const alpha = 0.3
+
+	view
+	.transform({
+		position:[0, origin.y, origin.z]
+	})
+	.grid({
+		axes: "zx",
+		divideX: 4,
+		divideY: 5,
+		niceX: false,
+		niceY: false,
+		width: lineWidth
+	})
+
+	view
+	.transform({
+		position:[2300, 0, origin.z]
+	})
+	.grid({
+		axes: "yz",
+		divideX: 4,
+		divideY: 4,
+		niceX: false,
+		niceY: false,
+		width: lineWidth
+	})
+}
+
+var mathbox = mathBox({
+  plugins: ['core', 'controls', 'cursor', 'stats'],
+  // plugins: ['VR', 'ui', 'controls'],
+  controls: {
+    klass: THREE.OrbitControls
+    // klass: THREE.VRControls
+  },
+});
+
+window._m = mathbox
+
+var three = mathbox.three;
+
+three.camera.position.set(-3.5, .4, 1.3);
+
+three.renderer.setClearColor(new THREE.Color(backgroundColor), 1.0);
+
+var cameraControl = new CameraControl(three.controls, chartScale)
+
+cameraControl.home();
+
+var draw=(datas)=>{
+	var data = datas.active
+	// debugger
+
+
+	// Mathbox view
+	var view = mathbox.cartesian({
+	  range: [chartRange.x, chartRange.y, chartRange.z],
+	  scale: chartScale,
+	});
+
+    // var camera = view.camera({
+    //   lookAt: [0, 0, 0],
+    // }, {
+    //   position: function (t) { 
+    //   	var _t = 0.1*t
+    //   	return [-3 * Math.cos(_t), .4 * Math.cos(_t * .381), -3 * Math.sin(_t)]
+    //   	.map(x=>{return 1.5*x + 1}) 
+    //   },
+    // });
+
+	var origin = {x: chartRange.x[0], y: chartRange.y[0], z: chartRange.z[0]}
+
+	// drawAxis(view, origin)
+	drawGrid(view, origin)
+
+	// color gradient for temperature curve
+	view.interval({
+		id:'tempratureColor',
+		width: numData,
+		channels: 4,
+		items: 1,
+		live: true,
+		expr: (emit, x, i, t)=>{
+			var min = 13
+			var max = 23
+			var val = _data.active.temperature[i]
+
+			var r0 = 1 - (val-min) / (max-min) // Green percentage
+			var r1 = 1 - r0
+
+			var c0 = [0.1, 0.7, 1] // Blue
+			var c1 = [1, 0.2, 0.1] // Red
+			var r = r0*c0[0]+r1*c1[0]
+			var g = r0*c0[1]+r1*c1[1]
+			var b = r0*c0[2]+r1*c1[2]
+			var a = 1.0-Math.pow(Math.sin(t*3), 16) + r0 + 0.2
+			if (x > Year) a *= 0.0
+			emit(r, g, b, a) // make it blink alarm at high temperature
+		}
+	})
+
+	// line alpha for co2 and balance curve
+	// controlling part of line this is visible to creating years marching forward effect
+	view.interval({
+		id:'lineAlpha',
+		width: numData,
+		channels: 4,
+		items: 1,
+		live: true,
+		expr: (emit, x, i, t)=>{
+			var a = x > Year ? 0.0 : 1.0
+			emit(1, 1, 1, a) // make it blink alarm at high temperature
+		}
+	})
+
+
+	if(!params.hideLegend){
+		// Draw year label
+	    view.array({
+	    	id: 'label-year',
+			data: [[
+				0.0 * chartRange.x[0] + 1.0 * chartRange.x[1], 
+				1.3 * chartRange.y[1] + (-0.3) * chartRange.y[0],
+				chartRange.z[0]
+			]],
+			channels: 3, // necessary
+			live: true,
+	    }).text({
+	    	id: 'label-year-text',
+	      data: ['Year'],
+	    }).label({
+	      color: 0xffffff,
+	      background: backgroundColor,
+	      size: 36*3,
+	      depth: 1
+	    });		
+
+	    var labelYearText = mathbox.select("#label-year-text")
+	}
+
+
+	var charts={}
+	charts['temperature'] = new Chart(mathbox, {
+		x : data.year,
+		y : data['temperature'],
+		z_offset : -10,
+		id : 'temperature',
+		xRange : chartRange.x,
+		yRange : [12, 24],
+		zRrange : chartRange.z,
+		scale : chartScale,
+		// color : 0xffcc44,
+		color : 0xffffff,
+	        colors : '#tempratureColor',
+	        //labelSize : labelSize,
+	        lineWidth : params.tempLineWidth,
+		labelFunc: (year, val)=>{
+		    //return [''+year+': '+val+'\u2103 increase']
+		    //var str = val+'\u2103 increase';
+		    var str = val+'\u2103';
+		    $("#tempVal").html(str);
+		    return [str]
+		}
+	})
+	charts['co2'] = new Chart(mathbox, {
+		x : data.year,
+		y : data['co2'],
+		z_offset : 0,
+		id : 'co2',
+		xRange : chartRange.x,
+		yRange : [0, 2200],
+		zRrange : chartRange.z,
+		scale : chartScale,
+		color : 0xaf8f30,
+		colors : '#lineAlpha',
+	        lineWidth : params.co2LineWidth,
+	        //labelSize : labelSize,
+		labelFunc: (year, val)=>{
+			//return [''+year+': '+val+'PPM increase']
+		    //var str = val+'PPM increase';
+		    var str = val+'PPM';
+		    $("#co2Val").html(str);
+		    return [str];
+		}
+	})
+	charts['balance'] = new Chart(mathbox, {
+		x : data.year,
+		y : data['balance'],
+		z_offset : -5,
+		id : 'balance',
+		xRange : chartRange.x,
+		yRange : [-1, 8],
+		zRrange : chartRange.z,
+		scale : chartScale,
+		color : 0x00ffff,
+		colors : '#lineAlpha',
+	        //labelSize : labelSize,
+	        lineWidth: params.balanceLineWidth,
+		labelFunc: (year, val)=>{
+		    //return [''+year+': '+val+' energy balance']
+		    //str = val + 'balance';
+		    str = ''+val;
+		    $("#energyVal").html(str);
+		    return [str];
+		}
+	})
+
+	// charts['ice'] 		 = plotLine('ice', [0, 10], 6.6, 0xffffff, null)
+	// charts['precipitation'] = plotLine('precipitation', [0.000032, 0.00004], 10,  0x00ff00, null)
+
+	var sands = new Sands(mathbox, {
+			x : data.year,
+			y : data['temperature'],
+			z_offset : -10,
+			id : 'sands',
+			xRange : chartRange.x,
+			yRange : [12, 24],
+			zRrange : chartRange.z,
+			scale : chartScale,
+			// color : 0xffcc44,
+			color : 0xffffff,
+			colors : '#tempratureColor'
+	})
+
+	three.on('update', ()=>{
+		TWEEN.update()
+
+		Object.keys(charts).forEach(id=>{
+			charts[id].update(data[id])
+		})
+		sands.update(data['temperature'])
+
+	        //if(!params.hideLegend)
+			//labelYearText.set('data', [Year])
+		labelYearText.set('data', [''])
+	})
+}
---
+++ sand/js/chart_v2.js
@@ -0,0 +1,321 @@
+"use strict"
+
+/*
+A few params can be set through the query string.  These are:
+
+labelSize           60  The size for all labels
+gridLineWidth       5   The thickness of lines on the zx and yz axes
+chartGridLineWidth  1   The thickness of the 'ticks' on each chart
+envelopeLineWidth   3   The thickness of upper and lower envelope lines
+refLineWidth        3   The thickness of the horizontal reference lines
+co2LineWidth        20  Thickness of CO2 graph
+tempLineWidth       20  Thickness of temp graph
+balanceLineWidth    10  Thickness of energy balance graph
+*/
+var params = {}
+
+params.labelSize = 10;
+params.gridLineWidth = 5;
+params.chartGridLineWidth = 3;
+params.envelopeLineWidth = 3;
+params.refLineWidth = 3;
+params.co2LineWidth = 20
+params.tempLineWidth = 60
+params.balanceLineWidth = 30
+params.hideLegend = false
+params.showPanel = true;
+
+class Chart{
+	constructor(mathbox, options){
+		this.mathbox = mathbox
+		this.x = options.x
+		this.y = options.y
+		this.z_offset = options.z_offset
+		this.id = options.id
+		this.xRange = options.xRange
+		this.yRange = options.yRange
+		this.zRrange = options.zRrange
+		this.scale = options.scale
+		this.color = options.color
+		this.colors = options.colors
+	        this.lineWidth = options.lineWidth || 20
+		this.labelFunc = options.labelFunc || ((val)=>{return [val]})
+	        //this.labelSize = options.labelSize || 36
+	        this.labelSize = params.labelSize
+		this.chart = null
+
+		this.init()
+	}
+
+	init(){
+		// debugger
+		// trun z_offset into array
+		this.z = this.x.map(()=>{return this.z_offset})
+
+		var data = _.zip(this.x, this.y, this.z)
+		var reference = [[this.x[0], this.y[0], this.z_offset],
+			[_.last(this.x), this.y[0], this.z_offset]]
+
+		var view = this.mathbox.cartesian({
+		  range: [this.xRange, this.yRange, this.zRrange],
+		  scale: this.scale
+		});
+
+
+		// draw line
+		view.array({
+		  id: this.id,
+		  width: numData,
+		  data: data,
+		  items: 1,
+		  channels: 3,
+		  live: false
+		}).line({
+			id: this.id+'-line',
+			opacity: 1.0,
+			color: this.color,
+			colors: this.colors,
+			width: this.lineWidth
+		})
+
+		// draw current year mark as a point
+		view.array({
+			id: this.id+'-lineend-point-pos',
+			width: 1,
+			data: [[1850,0,0]],
+			items: 1,
+			channels: 3,
+			live: true
+		}).point({
+			id: this.id+'-lineend-point-mark',
+			opacity: 0.3,
+			color: this.color,
+			size: 60
+		})
+
+		view.array({
+			id: this.id+'-lineend-point-pos-b',
+			width: 1,
+			data: [[1850,0,0]],
+			items: 1,
+			channels: 3,
+			live: true
+		}).point({
+			id: this.id+'-lineend-point-mark-b',
+			opacity: 1,
+			color: 0xffffff,
+			size: 20
+		})
+
+	    this.lineendMarkPos = this.mathbox.select('#'+this.id+'-lineend-point-pos')
+	    this.lineendMarkPosB = this.mathbox.select('#'+this.id+'-lineend-point-pos-b')
+
+
+		var dataRCP8p5 = _.zip(this.x, _data.rcp8p5[this.id], this.z)
+		// draw line
+		view.array({
+		  id: this.id+'-rcp8p5',
+		  width: numData,
+		  data: dataRCP8p5,
+		  items: 1,
+		  channels: 3,
+		  live: false
+		}).line({
+			id: this.id+'-rcp8p5-line',
+			opacity: 0.5,
+			zIndex: 10,
+			color: this.color,
+			// colors: this.colors,
+			//width: 1
+			width: params.envelopeLineWidth
+		})
+
+		var dataRCP2p6 = _.zip(this.x, _data.rcp2p6[this.id], this.z)
+		// draw line
+		view.array({
+		  id: this.id+'-rcp2p6',
+		  width: numData,
+		  data: dataRCP2p6,
+		  items: 1,
+		  channels: 3,
+		  live: false
+		}).line({
+			id: this.id+'-rcp2p6-line',
+			opacity: 0.5,
+			zIndex: 10,
+			color: this.color,
+			// colors: this.colors,
+			//width: 1
+			width: params.envelopeLineWidth
+		})
+
+
+		// draw reference line
+		view.array({
+		  id: this.id+'-reference',
+		  width: 2,
+		  data: reference,
+		  items: 1,
+		  channels: 3,
+		  live: false
+		}).line({
+			id: this.id+'-line-reference',
+			color: this.color,
+			colors: this.colors,
+			//width: 5
+			width: params.refLineWidth
+		})
+
+
+		this.chart =this.mathbox.select("#"+this.id)
+
+		// draw XY grid
+		view.transform({position:[0, 0, this.z_offset]})
+		.grid({
+			axes: "xy",
+			divideX: 4,
+			divideY: 4,
+			niceX: false,
+			niceY: false,
+			width: params.chartGridLineWidth,
+			opacity: 0.3,
+			color: this.color
+		})
+
+		// Draw X axis
+		view.transform({
+			position:[0, this.yRange[0], this.z_offset]
+		}).axis({
+		  axis: "x",
+		  end: false,
+		  width: 6,
+		  depth: 1,
+		  color: this.color,
+		  opacity: .5,
+		})
+		
+		// Draw Y axis
+		view.transform({
+			position:[this.xRange[1], 0, this.z_offset]
+		}).axis({
+		  axis: "y",
+		  end: true,
+		  width: 6,
+		  depth: 1,
+		  color: this.color,
+		  opacity: .5,
+		})
+
+		// Draw Y axis labels and ticks
+		view.scale({
+	      divide: 4,
+	      origin: [this.xRange[1], this.yRange[0], this.z_offset],
+	      axis: "y",
+	      nice: false
+	    })
+	    .ticks({
+	      classes: ['foo', 'bar'],
+	      width: 10
+	    })
+
+	    if(!params.hideLegend){
+		    view.text({
+		    	live: false,
+		    	depth: 2,
+		    	data: interpolate(this.yRange[0], this.yRange[1], 5)
+		    })
+		    .label({
+		    	color: this.color,
+		    	background: backgroundColor,
+		    	//size: 36,
+		    	size: this.labelSize,
+		    	depth: 1
+		    	// offset: [1,1]
+		    })
+
+		    // Y axis id
+	        view.array({
+				data: [[this.xRange[1], 0.1*(this.yRange[1]-this.yRange[0]) + this.yRange[1], this.z_offset]],
+				channels: 3, // necessary
+				live: false,
+		    }).text({
+		      data: [this.id],
+		      depth: 2
+		    }).label({
+		      color: this.color,
+		      background: backgroundColor,
+		      snap: false,
+		      //size: 48,
+	              size: this.labelSize,
+		      depth: 1,
+		      zIndex: 1
+		    });		
+
+		    // projection at 2300
+	        view.array({
+	        	id: this.id+'-label-position',
+				data: [[this.xRange[1], 0.1*(this.yRange[1]-this.yRange[0]) + this.yRange[1], this.z_offset]],
+				channels: 3, // necessary
+				live: true,
+		    }).text({
+		    	id: this.id+'-label-text',
+		      data: [0],
+		    }).label({
+		      color: this.color,
+		      background: backgroundColor,
+		      //size: 36,
+		      size: this.labelSize,
+		      depth: 1
+		    });		
+
+		    // Current value
+	        view.array({
+	        	id: this.id+'-label-year-position',
+				data: [[Year, 0.1*(this.yRange[1]-this.yRange[0]) + this.yRange[1], this.z_offset]],
+				channels: 3, // necessary
+				live: true,
+		    }).text({
+		    	id: this.id+'-label-year-text',
+		      data: ['temperature'],
+		    }).label({
+		      color: this.color,
+		      background: backgroundColor,
+		      //size: 36,
+	              size: this.labelSize,
+		      depth: 1
+		    });	
+
+		    this.labelPosition = this.mathbox.select('#'+this.id+'-label-position')
+		    this.labelText = this.mathbox.select('#'+this.id+'-label-text')
+		    this.labelYearPos = this.mathbox.select('#'+this.id+'-label-year-position')
+		    this.labelYearText = this.mathbox.select('#'+this.id+'-label-year-text')
+		}
+	}
+
+	update(y){
+		var newData=_.zip(this.x, y, this.z)
+		// this.chart =this.mathbox.select("#"+this.id)
+		this.chart.set('data', newData)
+
+		this.lineendMarkPos.set('data', 
+			[[Year, y[Year-1850], this.z_offset]]
+			)
+
+		this.lineendMarkPosB.set('data', 
+			[[Year, y[Year-1850], this.z_offset]]
+			)
+	        $("#yearText").html(Year);
+	        $("#yearLabel").html(Year);
+		if(!params.hideLegend){
+			this.labelPosition.set('data', [[this.xRange[1], y[numData-1], this.z_offset]])
+
+			this.labelText.set('data', [y[numData-1].toPrecision(3)])
+
+			this.labelYearPos.set('data', 
+				[[Year, 0.1*(this.yRange[1]-this.yRange[0]) + y[Year-1850], this.z_offset]])
+			this.labelYearText.set('data', 
+				this.labelFunc(Year, (y[Year-1850] - PreIndustrial[this.id]).toFixed(1)))
+		}
+	}
+
+}
