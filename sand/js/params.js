9fb85d2f3197415d6cc9e71afc263501fa88358b
Yulius Tjahjadi <yultide@yahoo.com>
2017-06-22 19:34:14 -0700

    add capture tab

--- sand/js/params.js
+++ sand/js/params.js
@@ -19,10 +19,24 @@ var paramsDefault = {
         z: '#0074D9',
         bg: '#303025',
         // bg: '#FFFFFF'
+    },
+
+    capturer: {
+        width: 3840,
+        height: 2160,
+        framerate: 30,
+        format: 'webm-mediarecorder',
+        timeLimit: null,
+        startTime: null,
+        display: false
     }
 }
 
 var params = paramsDefault;
+var storedParams = localStorage.getItem('params');
+if (storedParams) {
+    params = JSON.parse(storedParams);
+}
 
 
 var container = document.getElementById("jsoneditor");
@@ -46,9 +60,18 @@ $('#config-reset').click(function() {
 
 $(document.body).keyup(function(e) {
     if (e.keyCode == 32) {
+        // space
         editor.set(params);
         editor.expandAll();
         $('#config').modal();
+    } else if (e.keyCode == 88) {
+        // X - stop recording
+        if (capturer) {
+            three.off('update', renderCapture);
+            capturer.stop();
+            capturer.save();
+            capturer = null;
+        }
     }
 });
 
@@ -56,4 +79,60 @@ function updateParams(newParams) {
     params = newParams;
     localStorage.setItem('params', JSON.stringify(params));
     resetApp();
+}
+
+// recording
+
+var capturer;
+
+$('#config').on('show.bs.modal', function() {
+    var form = $('#recording-form');
+    form.find('[name=framerate]').val(params.capturer.framerate);
+    form.find('[name=format]').val(params.capturer.format);
+    form.find('[name=timeLimit]').val(params.capturer.timeLimit);
+    form.find('[name=startTime]').val(params.capturer.startTime);
+    form.find('[name=display]').prop('checked', params.capturer.display);
+});
+
+$('#start-recording').click(function() {
+    var form = $('#recording-form');
+    params.capturer.framerate = parseInt(form.find('[name=framerate]').val()) || paramsDefault.capturer.framerate;
+    params.capturer.format = form.find('[name=format]').val();
+    params.capturer.timeLimit = parseInt(form.find('[name=timeLimit]').val()) || 0;
+    params.capturer.startTime = parseInt(form.find('[name=startTime]').val()) || 0;
+    params.capturer.display = form.find('[name=display]').prop('checked');
+
+    var options = {
+        framerate: params.capturer.framerate,
+        format: params.capturer.format,
+        timeLimit: params.capturer.timeLimit,
+        timeStart: params.capturer.timeStart,
+        display: params.capturer.display
+    };
+
+    // force resize
+    var w = params.capturer.width;
+    var h = params.capturer.height;
+    three.trigger({
+        type: 'resize',
+        renderWidth: w,
+        renderHeight: h,
+        viewWidth: w,
+        viewHeight: h,
+        aspect: w/h,
+        pixelRatio: 1
+    });
+    $(three.renderer.domElement).css({width: w, height: h});
+
+    capturer = new CCapture(options);
+    capturer.start();
+
+    three.off('update', renderCapture);
+    three.on('update', renderCapture);
+});
+
+function renderCapture() {
+    if (capturer && three) {
+        capturer.capture(three.renderer.domElement);
+    }
 }
